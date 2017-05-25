c79f144b9bdf1aa2c5c1fca1a4afff77f375191b
LMNTZZ <bervoetskevin@gmail.com>
2017-05-25 22:11:45 +0200

    Set Promise for Async service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -73,17 +73,19 @@ heroes: Hero[];
 })
 export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
+  
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
+  
   ngOnInit(): void {
     this.getHeroes();
   }
-
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
