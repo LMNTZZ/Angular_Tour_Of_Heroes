e81bcf1f2d765acb8e5bdc0f4ba7059ea05d66bc
LMNTZZ <bervoetskevin@gmail.com>
2017-05-25 22:00:02 +0200

    Make the Hero service serve the data to the App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 heroes: Hero[];
 
@@ -65,12 +68,21 @@ heroes: Hero[];
       margin-right: .8em;
       border-radius: 4px 0 0 4px;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes = HEROES;
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
