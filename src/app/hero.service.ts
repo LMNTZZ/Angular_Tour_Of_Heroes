0fb496e206fa806001a7ffd591876f86d35906d0
LMNTZZ <bervoetskevin@gmail.com>
2017-06-13 23:27:48 +0200

    Add ability to add heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -34,6 +34,14 @@ export class HeroService {
 
   private headers = new Headers({'Content-Type': 'application/json'});
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
+
   update(hero: Hero): Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
